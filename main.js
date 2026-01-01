50e59c59833123146341b958abcf4175450c6114
Edqwird <161136059+Edqwird@users.noreply.github.com>
2026-01-01 22:12:17 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 document.querySelector('.burgers').addEventListener('click', () => {
     document.querySelector('.burgers').style.display = 'none';
     document.querySelector('.noneLines').style.display = 'flex';
-    document.querySelector('.uiBar').style.height = '300px';
-    document.querySelector('.mobileBar').style.height = '330px';
+    document.querySelector('.uiBar').style.height = '360px';
+    document.querySelector('.mobileBar').style.height = '390px';
 });
 
 document.querySelector('.noneLines').addEventListener('click', () => {
